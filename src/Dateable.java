ca31b1df85b4c7aa018c76be861024fb4b05c338
samuel <samuelak2028@student.cis.edu.hk>
2025-09-12 11:33:32 +0800

    Changes scanner object name

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 public class Dateable {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String age;
         System.out.println("Enter your age: ");
-        age = myObj.nextLine();
+        age = sc.nextLine();
         int dateAge = 7+(Integer.parseInt(age))/2;
         System.out.println(age + "-year olds should date somebody who is at least " + dateAge + " years old.");
     }
 }
+d
--- src/Pages.java
+++ src/Pages.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class Pages {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String age;
         System.out.println("Enter your age:");
-        age = myObj.nextLine();
+        age = sc.nextLine();
         int pages = 100-Integer.parseInt(age);
         System.out.print(age + "-year olds should read at least " + pages + " pages before giving up on a book.");
     }
